970b21d792a8893a6adec11135d5742e00bc4e90
taichi <taichi.01gr14@gmail.com>
2023-02-02 08:08:43 +0900

    make test

---
+++ tests/Feature/ArticleControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * testIndex
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.index');
+    }
+
+    /**
+     * testGuestCreate
+     *
+     * @return void
+     */
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * testGuestCreate
+     *
+     * @return void
+     */
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
+    }
+}
